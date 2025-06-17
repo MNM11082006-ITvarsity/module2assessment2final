29b8f103eec33597592fde468f5ebb4632c9ea9c
MNM112006 <tanjirouzumaki82@gmail.com>
2025-06-17 11:33:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll("nav a");
+const current = location.pathname.split("/").pop();
+
+links.forEach(link => {
+  if (link.getAttribute("href") === current) {
+    link.classList.add("active");
+  }
+});
